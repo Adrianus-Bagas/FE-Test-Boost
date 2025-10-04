2261b765f1c9c3eceea676b7daff45f4687e2023
Adrianus-Bagas <bagasadrianus80@gmail.com>
2025-10-04 15:47:57 +0700

    feat: add post interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,13 @@ export interface ICategory {
   id: number;
   name: string;
 }
+
+export interface IPost {
+  id: number;
+  title: string;
+  author: string;
+  summary: string;
+  content: string;
+  category: ICategory;
+  createdAt: string;
+}
